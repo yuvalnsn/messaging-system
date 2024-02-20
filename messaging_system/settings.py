e20ef06fee3358bd967be606c1930e07bfe55cd9
yuval <yuvalnsn@gmail.com>
2024-02-20 02:15:55 +0200

    Configuring for production

--- messaging_system/settings.py
+++ messaging_system/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-55u63ir8qik7#w@16-x^e#v6x#7hhl74))ljme0d^_ykam73qr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://messaging-system-abra-506f0fd81b2d.herokuapp.com/']
 
